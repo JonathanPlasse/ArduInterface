0b053535d2584396461ce0644db18d7e393f7242
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2019-11-30 12:12:28 +0100

    Removed unnecessary code

--- pid_interface.py
+++ pid_interface.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
-from PyQt5.QtWidgets import (QWidget, QPushButton, QSpinBox, QDoubleSpinBox, QVBoxLayout, QLineEdit, QCheckBox, QSlider, QHBoxLayout, QFormLayout, QGroupBox, QApplication, QSizePolicy)
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import yaml
-from PyQt5 import QtCore
 import threading
 from binserial import BinSerial
 from beautifulplot import BeautifulPlot
 
-class PidInterface(QWidget):
+class SerialInterface:
     def __init__(self):
-        super().__init__()
         self.init_parameters()
         self.init_ui()
         # self.init_serial()
@@ -26,110 +21,15 @@ class PidInterface(QWidget):
     def init_parameters(self):
         with open('config.yml', 'r') as config_yml:
             config = yaml.safe_load(config_yml)
+
+        self.serial = config['serial']
         self.port_name = config['port_name']
         self.baud_rate = config['baud_rate']
-        self.kp = config['kp']
-        self.ki = config['ki']
-        self.kd = config['kd']
-        self.sample_time = config['sample_time']
-        self.max_setpoint = config['max_setpoint']
-        self.mode = config['mode']
-        self.anti_windup = config['anti_windup']
 
-    def set_sample_time(self, new_sample_time):
-        self.sample_time = new_sample_time
 
-    def set_kp(self, new_kp):
-        self.kp = new_kp
-
-    def set_ki(self, new_ki):
-        self.ki = new_ki
-
-    def set_kd(self, new_kd):
-        self.kd = new_kd
-
-    def set_setpoint(self, new_setpoint):
-        self.setpoint = new_setpoint
-        self.sent_parameters()
-
-    def set_mode(self, new_mode):
-        self.mode = new_mode
-
-    def set_anti_windup(self, new_anti_windup):
-        self.anti_windup = new_anti_windup
 
     def init_ui(self):
-        self.setWindowTitle('PidInterface')
-
-        # Parameters
-        self.sample_time_spin = QSpinBox()
-        self.sample_time_spin.setMinimum(0)
-        self.sample_time_spin.setMaximum(1000)
-        self.sample_time_spin.setSuffix(' ms')
-        self.sample_time_spin.setValue(self.sample_time)
-        self.sample_time_spin.valueChanged.connect(self.set_sample_time)
-
-        self.kp_spin = QDoubleSpinBox()
-        self.kp_spin.setMinimum(0)
-        self.kp_spin.setMaximum(float('inf'))
-        self.kp_spin.setValue(self.kp)
-        self.kp_spin.valueChanged.connect(self.set_kp)
-
-        self.ki_spin = QDoubleSpinBox()
-        self.ki_spin.setMinimum(0)
-        self.ki_spin.setMaximum(float('inf'))
-        self.ki_spin.setValue(self.ki)
-        self.ki_spin.valueChanged.connect(self.set_ki)
-
-        self.kd_spin = QDoubleSpinBox()
-        self.kd_spin.setMinimum(0)
-        self.kd_spin.setMaximum(float('inf'))
-        self.kd_spin.setValue(self.kd)
-        self.kd_spin.valueChanged.connect(self.set_kd)
-
-        self.setpoint_slider = QSlider(QtCore.Qt.Horizontal)
-        self.setpoint_slider.setMinimum(0)
-        self.setpoint_slider.setMaximum(self.max_setpoint)
-        self.setpoint_slider.setValue(0)
-        self.setpoint_slider.sliderMoved.connect(self.set_setpoint)
-
-        self.mode_check = QCheckBox()
-        self.mode_check.setChecked(self.mode)
-        self.mode_check.toggled.connect(self.set_mode)
-
-        self.anti_windup_check = QCheckBox()
-        self.anti_windup_check.setChecked(self.anti_windup)
-        self.anti_windup_check.toggled.connect(self.set_anti_windup)
-
-        parameters_layout = QFormLayout()
-        parameters_layout.addRow('sample_time', self.sample_time_spin)
-        parameters_layout.addRow('kp', self.kp_spin)
-        parameters_layout.addRow('ki', self.ki_spin)
-        parameters_layout.addRow('kd', self.kd_spin)
-        parameters_layout.addRow('setpoint', self.setpoint_slider)
-        parameters_layout.addRow('mode', self.mode_check)
-        parameters_layout.addRow('anti_windup', self.anti_windup_check)
-
-        parameters_group = QGroupBox('Parameters')
-        parameters_group.setLayout(parameters_layout)
-
-        apply_button = QPushButton('Apply')
-        apply_button.clicked.connect(self.sent_parameters)
-
-        pid_layout = QVBoxLayout()
-        pid_layout.addWidget(parameters_group)
-        pid_layout.addWidget(apply_button)
-        pid_layout.addStretch()
-
-        # Display
         self.plot = BeautifulPlot(self)
-
-        # Main
-        main_layout = QHBoxLayout()
-        main_layout.addLayout(pid_layout)
-        main_layout.addWidget(self.plot)
-
-        self.setLayout(main_layout)
 
     def sent_parameters(self):
         """run the step response and get the measures"""
@@ -149,7 +49,4 @@ class PidInterface(QWidget):
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    pid_interface = PidInterface()
-    pid_interface.show()
-    sys.exit(app.exec_())
+    SerialInterface()
